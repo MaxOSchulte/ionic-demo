545ba9668fe92efbec705261548d38123c5ecc18
Max Schulte <max.schulte@thinktecture.com>
2021-01-21 16:37:13 +0100

    solution lab 5

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
 
 @Injectable({
   providedIn: 'root',
@@ -6,12 +8,22 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   private authenticated = false;
 
-  constructor() {}
+  constructor(private readonly platform: Platform, private readonly fingerprintAio: FingerprintAIO) {
+  }
 
   async authenticate(): Promise<boolean> {
     try {
       // TODO implement some authentication logic. E.g. FingerprintAIO
-      this.authenticated = true;
+      if (this.platform.is('hybrid') && (await this.fingerprintAio.isAvailable())) {
+        this.authenticated = await this.fingerprintAio.show({
+          title: 'Please authenticate',
+          description: 'Face ID and Fingerprint authentication',
+        });
+      } else {
+        // Fallback to e.g. web authentication
+        this.authenticated = true;
+      }
+
     } catch (e) {
       console.error('authentication failed', e);
     }
