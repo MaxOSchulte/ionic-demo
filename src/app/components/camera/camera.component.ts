fdec398cd589a8985eca49c8b0e61dd9b0340636
Max Schulte <max.schulte@thinktecture.com>
2022-07-06 15:52:50 +0200

    style: whitespace

--- src/app/components/camera/camera.component.ts
+++ src/app/components/camera/camera.component.ts
@@ -5,45 +5,48 @@ import { PictureService } from 'src/app/services/picture.service';
 // A good example how to build an app and use the camera
 // https://ionicframework.com/docs/angular/your-first-app
 @Component({
-  selector: 'app-camera',
-  templateUrl: './camera.component.html',
-  styleUrls: ['./camera.component.scss'],
+    selector: 'app-camera',
+    templateUrl: './camera.component.html',
+    styleUrls: ['./camera.component.scss'],
 })
 export class CameraComponent {
-  @ViewChild('photoRef', { static: true }) photoInput: ElementRef;
-  @Output() public readonly capture = new EventEmitter<string>();
+    @ViewChild('photoRef', {static: true}) photoInput: ElementRef;
+    @Output() public readonly capture = new EventEmitter<string>();
 
-  constructor(private readonly picture: PictureService, private readonly toasts: ToastController) {}
-
-  initPictureProcess() {
-    // fake click on photo input to open the dialog
-    this.photoInput.nativeElement.click();
-  }
-
-  async handleInput() {
-    // get selected files
-    const files: FileList = this.photoInput.nativeElement.files;
-    if (!files && files.length <= 0) {
-      return;
+    constructor(private readonly picture: PictureService, private readonly toasts: ToastController) {
     }
-    try {
-      // only convert the first selected photo to base64 and emit
-      const base64Photo = await this.picture.convertToBase64(files[0]);
-      this.capture.emit(base64Photo);
-    } catch {
-      // notify user on error
-      const toast = await this.toasts.create({
-        message: 'Could not take picture',
-        duration: 2000,
-        color: 'warning',
-        translucent: true,
-      });
-      toast.present();
-    }
-  }
 
-  async takePictureCapacitor() {
-    const picture = await this.picture.takePicture();
-    this.capture.emit(picture);
-  }
+    initPictureProcess() {
+        // fake click on photo input to open the dialog
+        this.photoInput.nativeElement.click();
+    }
+
+    async handleInput() {
+        // get selected files
+        const files: FileList = this.photoInput.nativeElement.files;
+        if (!files && files.length <= 0) {
+            return;
+        }
+        try {
+            // only convert the first selected photo to base64 and emit
+            const base64Photo = await this.picture.convertToBase64(files[0]);
+            this.capture.emit(base64Photo);
+        } catch {
+            // notify user on error
+            const toast = await this.toasts.create({
+                message: 'Could not take picture',
+                duration: 2000,
+                color: 'warning',
+                translucent: true,
+            });
+            toast.present();
+        }
+    }
+
+    async takePictureCapacitor() {
+        const picture = await this.picture.takePicture();
+        if (picture) {
+            this.capture.emit(picture);
+        }
+    }
 }
--- src/app/pages/fav-list/fav-list.page.ts
+++ src/app/pages/fav-list/fav-list.page.ts
@@ -1,23 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { OfferItem } from '../../models/offer-item.model';
 import { FavouritesService } from '../../services/favourites.service';
+import { ViewWillEnter } from '@ionic/angular';
 
 @Component({
-  selector: 'app-fav-list',
-  templateUrl: './fav-list.page.html',
-  styleUrls: ['./fav-list.page.scss'],
+    selector: 'app-fav-list',
+    templateUrl: './fav-list.page.html',
+    styleUrls: ['./fav-list.page.scss'],
 })
-export class FavListPage implements OnInit {
-  items$: Promise<OfferItem[]>;
+export class FavListPage implements ViewWillEnter {
+    items$: Promise<OfferItem[]>;
 
-  constructor(private readonly favourites: FavouritesService) {}
+    constructor(private readonly favourites: FavouritesService) {
+    }
 
-  // add enter callback to load / refresh items
-  async ionViewWillEnter() {
-    this.ngOnInit();
-  }
-
-  async ngOnInit() {
-    this.items$ = this.favourites.getAll();
-  }
+    // add enter callback to load / refresh items
+    async ionViewWillEnter() {
+        this.items$ = this.favourites.getAll();
+    }
 }
